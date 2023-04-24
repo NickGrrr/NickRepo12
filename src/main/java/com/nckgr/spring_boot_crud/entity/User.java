5427d9667e86b06ea1787afca6938eaf4b7ca48a
Nick <xStatiXx@yandex.ru>
2023-04-24 22:40:02 +0300

    Внесены исправления

--- src/main/java/com/nckgr/spring_boot_crud/entity/User.java
+++ src/main/java/com/nckgr/spring_boot_crud/entity/User.java
@@ -1,6 +1,11 @@
 package com.nckgr.spring_boot_crud.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import java.util.Objects;
 
 @Entity
